2be59403797dfcf2883b8951ac99e58800124e1c
John Mount <jmount@win-vector.com>
2017-11-02 07:22:32 -0700

    add htmls

---
+++ SparklyRMachineLearning/Exercises/solutions/mkHtml.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pandoc -i 01-Universal-tools.md -o 01-Universal-tools.html
+pandoc -i 02-Big-Data.md -o 02-Big-Data.html
+pandoc -i 03a-Spark-SQL.md -o 03a-Spark-SQL.html
+pandoc -i 03b-Spark-SQL.md -o 03b-Spark-SQL.html
+pandoc -i 04-Spark-ML.md -o 04-Spark-ML.html
+pandoc -i 05-Spark-Examples.md -o 05-Spark-Examples.html
+pandoc -i 06-Spark-Extension.md -o 06-Spark-Extension.html
